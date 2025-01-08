1bde045e8ae4df9c525dd13f76128f867de84bbd
theNorm <kisook2557@gmail.com>
2025-01-08 16:19:01 +0900

    타입 지정 방식, 명시적 타입 지정을 권장함

--- src/basic/basic.ts
+++ src/basic/basic.ts
@@ -25,3 +25,29 @@ function flipCoin() {
     // Meant to be Math.random()
     return Math.random() < 0.5;
 }
+
+// 1. 추론적 타입 지정
+let x = 3;
+// x = [0, 1, null];
+
+class Animal {}
+class Rhino extends Animal {
+    hasHorn: boolean = true;
+}
+class Elephant extends Animal {
+    hasTrunk: boolean = true;
+}class Snake extends Animal {
+    hasLegs: boolean = false;
+}
+
+let zoo = [new Rhino(), new Elephant(), new Snake()];
+// Animal[]
+
+// 2. 명시적 타입 지정(Explict Types)
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person:string, data:Date) {
+    console.log(`Hello ${person}, today is ${date}"`);
+}
+
+greet("Brendan", new Date());
