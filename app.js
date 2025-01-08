5a1650df0a6d308e0f19935987edd91b9829bae8
theNorm <kisook2557@gmail.com>
2025-01-08 12:13:27 +0900

    순수 자바스크립의 동적 타입 지정 예시시

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 console.log("Hello")
+
+let a = 10; // 숫자, number
+let b = "Hello"; // 문자열, string
+let c = true; // false, Bollean 참, 거짓
+let d = [1, 2, 3, 4]; // 배열, Array
+let e = { name : "Tom", age : 35}; // 객체, Object
+//..
+
+b = 50;
+
+//..
+console.log("a=" + a, ", type of " + typeof a)
+console.log("b=" + b, ", type of " + typeof b)
+console.log("c=" + c, ", type of " + typeof c)
+console.log("d[0]=" + d[0], ", type of " + typeof d)
+console.log("e.name=" + e.name, ", e.age=" + e.age, ", type of " + typeof e)
+
+// 예시
+
+let columnLength = 10; // 길이를 cm단위며, number로 지정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+columnLength = "30cm";
+
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+let addLength = columnLength + 5;
+let meterLength = addLength / 10;
+
+//.. 개발 진행 과정
+//.. 개발 진행 과정
+//.. 개발 완료
+console.log(addLength)
+console.log(meterLength)
