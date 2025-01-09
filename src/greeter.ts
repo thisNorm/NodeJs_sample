24b5aae05310bf5876ce79db46c754b7a326f82a
theNorm <kisook2557@gmail.com>
2025-01-09 10:13:02 +0900

    타입스크립의 타입 지정 방식식

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,8 +1,8 @@
-function greeter(person: string) {
+function greeter1(person: string) {
     return "Hello, " + person;
 }
 
 let user = "Inyong";
 
-console.log(greeter(user));
+console.log(greeter1(user));
 //document.body.textContent = greeter(user);
