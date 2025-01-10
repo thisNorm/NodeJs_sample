cd2b2314599664409e0c1a66bbcfbdde388fd616
theNorm <kisook2557@gmail.com>
2025-01-10 15:21:50 +0900

    타입스크립트의 모듈 export/import

---
+++ src/module/mathOperations.ts
@@ -0,0 +1,19 @@
+// 4 basic operating functions
+function add(a: number, b: number): number {
+    return a + b;
+}
+function substract(a: number, b: number): number {
+    return a - b;
+}
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+function divide(a: number, b: number): number {
+    return a / b;
+}
+
+export function divide2(a: number, b: number): number {
+    return a/ b;
+}
+
+export { add, substract, multiply, divide }
---
+++ src/module/module.ts
@@ -0,0 +1,6 @@
+import { add, substract, multiply, divide } from "./mathOperations";
+
+console.log("더하기 : " + add(1, 5));
+console.log("빼기 : " + substract(1, 5));
+console.log("곱하기 " + multiply(1, 5));
+console.log("나누기 " + divide(1, 5));
